78b7cf5fa1de20f088da72da2befb9f803445add
cajty <fdshjgjgffjhd@gmail.com>
2025-01-05 22:49:57 +0100

    fix that problem of that photo Abd

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -9,7 +9,7 @@ export function Navbar() {
 
   const handleDownloadCV = () => {
     // Replace this URL with your actual CV file URL
-    const cvUrl = '/public/belyamane_ayoub_JAVAANGULAR_JA.pdf (2).pdf';
+    const cvUrl = '../../../public/belyamane_ayoub_JAVAANGULAR_JA.pdf (2).pdf';
     window.open(cvUrl, '_blank');
   };
 
